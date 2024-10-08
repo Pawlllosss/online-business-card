5cb5e830b35c11db85ff7587ce64d02cda77ddda
Pawlllosss <oczadlypawe@gmail.com>
2024-10-08 20:50:43 +0200

    slowly getting there. Placed text and image horizontally.

--- src/Content.tsx
+++ src/Content.tsx
@@ -2,7 +2,7 @@ import Info from "./Info.tsx";
 import ImageHolder from "./ImageHolder.tsx";
 
 const Content = () => {
-    return <div className="min-h-screen grid place-content-center">
+    return <div className="min-h-screen flex flex-row justify-evenly items-center">
         <Info/>
         <ImageHolder/>
     </div>
--- src/Info.tsx
+++ src/Info.tsx
@@ -1,5 +1,5 @@
 const Info = () => {
-    return <>
+    return <div className="flex-col">
         <h1 id='engineer-name'
             className="text-2xl text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
             Paweł Oczadly
@@ -11,7 +11,7 @@ const Info = () => {
         <p>Java and Kotlin developer with some TypeScript and React expertise.</p>
         <p>Working in IT industry since 2017.</p>
         <p>Located in Poland - Kraków.</p>
-    </>;
+    </div>;
 }
 
 export default Info;
