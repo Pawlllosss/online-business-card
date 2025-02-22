3a03972bc1553ce171d60d9598f74e15501057a3
Pawlllosss <oczadlypawe@gmail.com>
2025-02-22 23:12:29 +0100

    Something of trying to setup styling for image

--- src/ImageHolder.tsx
+++ src/ImageHolder.tsx
@@ -1,8 +1,7 @@
 const ImageHolder = () => {
-    return <div className="flex-1">
-        IMAGE HERE
-        {/*<img src="https://oczadly-business-card.s3.amazonaws.com/oczadly-pic.jpg"*/}
-        {/*     className="w-full h-full object-cover rounded-lg shadow-lg	s"/>*/}
+    return <div className="flex">
+        <img src="https://oczadly-business-card.s3.amazonaws.com/oczadly-pic.jpg"
+             className="object-contain h-full w-full object-containrounded-lg shadow-lg	s"/>
     </div>
 }
 
--- src/Info.tsx
+++ src/Info.tsx
@@ -1,16 +1,27 @@
 const Info = () => {
-    return <div className="flex-1">
+    return <div className="flex flex-col justify-between">
         <h1 id='engineer-name'
             className="text-2xl text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
             Paweł Oczadly
         </h1>
-        <h1 id='engineer-name'
-            className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
-            Software Engineer
-        </h1>
-        <p>Java and Kotlin developer with some TypeScript and React expertise.</p>
-        <p>Working in IT industry since 2017.</p>
-        <p>Located in Poland - Kraków.</p>
+        <div>
+            <h1 id='engineer-name'
+                className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
+                Software Engineer
+            </h1>
+            <div>
+                Java and Kotlin developer with some TypeScript and React expertise. Working in IT industry since 2017.
+                Located in Poland - Kraków.
+            </div>
+        </div>
+        <div className="flex justify-evenly mt-8 space-x-8">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                My LinkedIn
+            </button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                My GitHub
+            </button>
+        </div>
     </div>;
 }
 
