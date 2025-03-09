3634357d90c3b6865595d10073c6cafbab4add90
Pawlllosss <oczadlypawe@gmail.com>
2025-03-09 22:40:06 +0100

    Adjusted icons on buttons, added rounding to image

--- src/ImageHolder.tsx
+++ src/ImageHolder.tsx
@@ -1,7 +1,7 @@
 const ImageHolder = () => {
     return <div className="flex">
         <img src="https://oczadly-business-card.s3.amazonaws.com/oczadly-pic.jpg"
-             className="object-contain h-full w-full object-containrounded-lg shadow-lg	s"/>
+             className="object-contain h-full w-full object-containrounded-lg rounded-t-3xl"/>
     </div>
 }
 
