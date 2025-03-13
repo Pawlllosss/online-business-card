5718084cc865278e30d15cff46ad6ba2a422b76e
Pawlllosss <oczadlypawe@gmail.com>
2025-03-13 23:33:11 +0100

    Improved text formatting. Updated website title.

--- src/Content.tsx
+++ src/Content.tsx
@@ -2,7 +2,7 @@ import Info from "./Info.tsx";
 import ImageHolder from "./ImageHolder.tsx";
 
 const Content = () => {
-    return <div className="flex items-stretch justify-center flex-row space-x-6 mx-52">
+    return <div className="flex h-[60vh] items-stretch justify-center flex-row space-x-6 mx-52">
         {/*<div className="flex h-[60vh]">*/}
         {/*<div className="flex flex-row justify-evenly items-center w-full">*/}
         <Info/>
